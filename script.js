18e2b8772ae0dce1e817728ec220d279bc6466bf
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-11 01:48:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 //your JS code here. If required.
+const input=document.querySelector("#ip");
+const btn=document.querySelector("#btn");
+const output=document.querySelector("#output");
+
+btn.addEventListener("click",()=>{
+promiseAndTransform()
+.then(res=>{
+	output.innerText="Result: "+res;
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			resolve(res*2);
+		},2000);
+	});
+})
+.then(res=>{
+	output.innerText="Result: "+res;
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			resolve(res-3);
+		},1000);
+	});
+})
+.then(res=>{
+	output.innerText="Result: "+res;
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			resolve(res/2);
+		},1000);
+	});
+})
+.then(res=>{
+	output.innerText="Result: "+res;
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			resolve(res+10);
+		},1000);
+	});
+})
+.then(res=>{
+		output.innerText="Result: "+res;
+});
+});
+function promiseAndTransform() {
+	const val = parseInt(input.value); 
+	return new Promise((resolve)=>{
+		if (isNaN(val)) {
+			alert("Please enter a valid number");
+			return;
+		}
+		setTimeout(()=>{
+			resolve(parseInt(input.value));
+		},2000);
+	});
+}
